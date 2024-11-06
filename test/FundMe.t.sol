286294f393fd8d51110fcada099fa678afff032a
Springer <zxllks@yahoo.com>
2024-11-06 10:11:43 +0800

    test: Test, console, setUp, assertEq

---
+++ test/FundMe.t.sol
@@ -0,0 +1,18 @@
+// SPDX-license-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() public {
+        fundMe = new FundMe();
+    }
+
+    function test_IsMinimumUSDFive() public view {
+        console.log("test_IsMinimumUSDFive", fundMe.MINIMUM_USD());
+        assertEq(fundMe.MINIMUM_USD(), 5 * 1e18);
+    }
+}
