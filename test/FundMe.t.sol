50a367dfe6cad1f66dded184a6fdecd1ec5f752e
Springer <zxllks@yahoo.com>
2024-11-06 10:38:27 +0800

    test: who is the owner of fundMe

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -8,11 +8,18 @@ contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() public {
+        // I -> FundMeTest -> FundMe
         fundMe = new FundMe();
     }
 
-    function test_IsMinimumUSDFive() public view {
-        console.log("test_IsMinimumUSDFive", fundMe.MINIMUM_USD());
+    function test_MinimumDollarIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5 * 1e18);
+    }
+
+    function test_WhoIsTheOwner() public view {
+        // console.log("[sender] call the test contract", msg.sender);
+        // console.log("[this test contract] deploy fundMe", address(this));
+        // console.log("[owner] of fundMe contract", fundMe.i_owner());
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
