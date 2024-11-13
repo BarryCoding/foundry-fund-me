f157bad50508501e41a6c335f78f43cfa5562b6e
Springer <zxllks@yahoo.com>
2024-11-13 10:41:00 +0800

    test: Integration

--- test/unit/FundMe.t.sol
+++ test/unit/FundMe.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/test.sol";
-import {FundMe} from "../src/FundMe.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
